0e9b4df3f89502a7e2e66bbc3be8c2ad23467499
thunderbiscuit <thunderbiscuit@protonmail.com>
2026-05-06 08:56:57 -0400

    Add 1.0 and 2.0 directories for Kotlin

--- kotlin/2.0/2.getaddress.main.kts
+++ kotlin/2.0/2.getaddress.main.kts
@@ -1,10 +1,10 @@
 /**
- * bdk-jvm 1.2.0
+ * bdk-jvm 2.3.1
  *
- * Query an Electrum server for the total balance of a wallet using a descriptor.
+ * Get an unused address from a wallet.
  */
 
-@file:DependsOn("org.bitcoindevkit:bdk-jvm:1.2.0")
+@file:DependsOn("org.bitcoindevkit:bdk-jvm:2.3.1")
 
 import org.bitcoindevkit.Descriptor
 import org.bitcoindevkit.Wallet
@@ -12,11 +12,12 @@ import org.bitcoindevkit.ElectrumClient
 import org.bitcoindevkit.FullScanRequest
 import org.bitcoindevkit.Network
 import org.bitcoindevkit.KeychainKind
-import org.bitcoindevkit.Connection
+import org.bitcoindevkit.AddressInfo
+import org.bitcoindevkit.Persister
 
 val descriptor = Descriptor("wpkh(tprv8ZgxMBicQKsPf2qfrEygW6fdYseJDDrVnDv26PH5BHdvSuG6ecCbHqLVof9yZcMoM31z9ur3tTYbSnr1WBqbGX97CbXcmp5H6qeMpyvx35B/84h/1h/0h/0/*)", Network.REGTEST)
 val changeDescriptor = Descriptor("wpkh(tprv8ZgxMBicQKsPf2qfrEygW6fdYseJDDrVnDv26PH5BHdvSuG6ecCbHqLVof9yZcMoM31z9ur3tTYbSnr1WBqbGX97CbXcmp5H6qeMpyvx35B/84h/1h/0h/1/*)", Network.REGTEST)
-val db = Connection.newInMemory()
+val db = Persister.newInMemory()
 
 val wallet: Wallet = Wallet(descriptor, changeDescriptor, Network.REGTEST, db)
 val electrumClient: ElectrumClient = ElectrumClient("tcp://127.0.0.1:60401")
@@ -24,8 +25,5 @@ val fullScanRequest: FullScanRequest = wallet.startFullScan().build()
 val update = electrumClient.fullScan(fullScanRequest, 10uL, 10uL, false)
 wallet.applyUpdate(update)
 
-val balanceSatoshi = wallet.balance().total.toSat()
-println("Balance: $balanceSatoshi satoshi")
-
-val address = wallet.revealNextAddress(KeychainKind.EXTERNAL)
-println(address.address)
+val lastUnusedAddress: AddressInfo = wallet.revealNextAddress(KeychainKind.EXTERNAL)
+println("The last unused address for this wallet is ${lastUnusedAddress.address} at index ${lastUnusedAddress.index}")
